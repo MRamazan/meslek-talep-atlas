ec51703a54d7ecca3b815a2397820ff7f977e1f3
Ramazan <mramazantrn28@gmail.com>
2026-07-26 19:51:30 +0300

    Update site.config.mjs

--- site.config.mjs
+++ site.config.mjs
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  githubUsername: 'GITHUB_USERNAME',
-  repositoryName: 'REPOSITORY_NAME',
+  githubUsername: 'MRamazan',
+  repositoryName: 'meslek-talep-atlas',
   name: 'Meslek Talep Atlası',
   description: 'LinkedIn iş ilanlarından üretilmiş kategori ve meslek bazlı talep analizleri.',
 };
